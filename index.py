d87431e8408a7c4d6dba821560dbe7e8f6222261
D0ntP4nic42 <leozinngomes@gmail.com>
2025-03-16 12:34:01 -0300

    initial commit

---
+++ index.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Métodos Numéricos")
+st.write("Essa página tem como objetivo apresentar os códigos construidos no projeto de PIBIC de Métodos Numéricos da UnDF.")
+
+st.write("## Biblioteca de códigos:")
+st.write("A ideia do projeto é criar uma biblioteca de códigos em Python para Métodos Numéricos. A biblioteca será construída ao longo do projeto com diversos códigos. A seguir, temos a lista de códigos disponíveis:")
+st.write("1. Calculadora de determinante de matriz (Cofator)")
+st.write("2. ")
+
+st.write("## Calculadora de determinante de matriz (Cofator)")
+st.write("A calculadora de determinante de matriz é um código que calcula o determinante de uma matriz quadrada de qualquer ordem. O código utiliza o método de cofatores para realizar o cálculo.")
+
